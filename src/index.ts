16694e2dfb59e33965923532f13229c495ab6ba4
Anders Jaensson <anders.jaensson@gmail.com>
2021-10-04 15:47:43 +0200

    listen to push events

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on("push", async (context) => {
+
+    if (context.payload.ref === 'refs/heads/master'){
+      app.log.info(context);
+
+      //context.payload.commits[0]
+    }
+
+
+//    await context.octokit.issues.createComment(issueComment);
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
