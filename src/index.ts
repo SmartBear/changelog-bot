78ff7fa82f774da459368a86df3e82170ba9fafc
Matt Wynne <matt@cucumber.io>
2021-10-04 12:51:42 -0700

    Spike: Work out default branch. Read changelog.

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,24 @@ console.log("starting up...")
 
 export = (app: Probot) => {
   app.on("push", async (context) => {
-
-    console.log("got a push!")
     app.log.info(context);
-    if (context.payload.ref === 'refs/heads/master'){
 
-      //context.payload.commits[0]
+    if (context.payload.ref !== `refs/heads/${context.payload.repository.default_branch}`){
+      app.log.info(`Ignoring push to non-default-branch ${context.payload.ref}`)
+      return
     }
 
+    // TODO:
+    // 1. Read the body of the changelog file
+    const owner: string = context.payload.organization?.login || ""
+    const repo: string = context.payload.repository.name
+    const changelogBody = context.octokit.repos.getContent({ path: "CHANGELOG.md", owner, repo })
+
+    app.log.info(changelogBody)
+    
+    // 2. Parse it, to relate releases to issues
+    // 3. Comment on issues
 
 //    await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
