42825e6e5c56cd472f28c6a1b6052fa5d65f9960
Anders Jaensson <anders.jaensson@gmail.com>
2021-10-05 14:00:51 +0200

    only comment on issues we haven't already commented on

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Probot } from "probot";
 import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods'
-import { ChangeLog} from './model/ChangeLog'
+import { ChangeLog } from './model/ChangeLog'
 
 console.log("starting up...")
 
@@ -8,7 +8,7 @@ export = (app: Probot) => {
   app.on("push", async (context) => {
     app.log.info(context);
 
-    if (context.payload.ref !== `refs/heads/${context.payload.repository.default_branch}`){
+    if (context.payload.ref !== `refs/heads/${context.payload.repository.default_branch}`) {
       app.log.info(`Ignoring push to non-default-branch ${context.payload.ref}`)
       return
     }
@@ -28,24 +28,47 @@ export = (app: Probot) => {
     if ("content" in data) {
       const content = Buffer.from(data.content, "base64").toString()
       app.log.info(content)
-    
+
       // 2. Parse it, to relate releases to issues
       // -----------------------------------------
       const changeLog = await ChangeLog.parse(content)
 
       // 3. Comment on issues
       // --------------------
-      for(const release of changeLog.releases) {
+      for (const release of changeLog.releases) {
         // Do not add comments for unreleased issues (yet)
-        if (release.name.toLowerCase() === 'unreleased') {
+        if (release.name.toLowerCase().includes('unreleased')) {
           continue;
         }
-        for(const issue of release.issues) {
+        for (const issue of release.issues) {
+          const request: RestEndpointMethodTypes["issues"]["listComments"]["parameters"] = {
+            owner,
+            repo,
+            issue_number: issue.number
+          }
+
+          const allComments: any[] = await context.octokit.paginate(
+            context.octokit.issues.listComments,
+            request,
+            ({ data }) => data
+          );
+
+          const commentToAdd = `This was released in ${release.name}`;
+
+          const hasPreviousComment: boolean = allComments.some(comment => {
+            return comment.body === commentToAdd
+          });
+
+          if (hasPreviousComment) {
+            console.log("Not commenting on issue" + issue.number)
+            continue;
+          }
+
           const issueComment: RestEndpointMethodTypes["issues"]["createComment"]["parameters"] = {
             owner,
             repo,
             issue_number: issue.number,
-            body: `This was released in ${release.name}`
+            body: commentToAdd
           }
           await context.octokit.issues.createComment(issueComment);
         }
--- src/model/ChangeLog.ts
+++ src/model/ChangeLog.ts
@@ -7,37 +7,29 @@ export class ChangeLog {
   static async parse(content: string): Promise<ChangeLog> {
     const releases: Release[] = []
 
-    // TODO: how to make this thing sync?
-    await parseChangeLog({ text: content })
-      .then(function (result: any) {
-        result.versions.forEach((version: any) => {
-          console.log("found version", version)
-          const release = new Release(version.version || version.title, []);
+    try {
+      const result: object = await parseChangeLog({ text: content })
+      // @ts-ignore
+      result.versions.forEach((version: any) => {
+        const release = new Release(version.version || version.title, []);
 
-          let rowIssues = ChangeLog.findIssues(version);
-          release.issues.push(...rowIssues);
-          releases.push(release);
-        })
-        console.log("All releases:", JSON.stringify(releases, null, 2));
+        let rowIssues = ChangeLog.findIssues(version);
+        release.issues.push(...rowIssues);
+        releases.push(release);
       })
-      .catch(function (err: any) {
-        console.error(err)
-      });
-
-    console.log("releases", releases)
-
+    } catch (error) {
+      console.log(error)
+    }
     return new ChangeLog(releases)
   }
 
   private static findIssues(version: any) {
     let rowIssues: Issue[] = [];
     version.parsed._.forEach((row: any) => {
-      console.log("Row:", row);
       const re = new RegExp('(#\\d+)');
       const matches = re.exec(row)
       if (matches) {
         const issueNumber: number = parseInt(matches[0].replace("#", ""))
-        console.log("Found issue:", issueNumber);
         rowIssues.push(new Issue(issueNumber))
       }
     })
